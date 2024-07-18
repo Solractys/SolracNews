fcbbeac637069103b7020c3a4c1872b688747dd9
Solractys <carloseduardosilvasouzadelima@gmail.com>
2024-07-18 19:48:38 -0300

    with credentials

--- SolracNews/src/App.tsx
+++ SolracNews/src/App.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react"
-import { api } from "./lib/axios"
+// import { api } from "./lib/axios"
 import { format } from "date-fns"
 import { ptBR } from "date-fns/locale"
+import axios from "axios"
 
 
 interface News {
@@ -21,45 +22,55 @@ interface News {
 export default function App() {
   const [news, setNews] = useState<News[]>([]);
   async function getNews() {
-    const url = `https://newsapi.org/v2/top-headlines?category=technology&country=br&pagesize=30&apiKey=e96df1f3a9374775bf85c39ec8bf5982`;
-
-    await api.get(url)
-    .then((response) => {
-        setNews(response.data.articles)
+    // const url = `https://newsapi.org/v2/top-headlines?category=technology&country=br&pagesize=30&apiKey=e96df1f3a9374775bf85c39ec8bf5982`;
+    // await axios.get(url)
+    axios.get('https://newsapi.org/v2/top-headlines', {
+      params: {
+        category: 'technology',
+        country: 'br',
+        pagesize: 30,
+        apiKey: 'e96df1f3a9374775bf85c39ec8bf5982'
+      },
+      headers: {
+        'Content-Type': 'application/json',
+      }
     })
-  } 
-  useEffect(() => {getNews()}, [])
-  
+      .then((response) => {
+        setNews(response.data.articles)
+      })
+  }
+  useEffect(() => { getNews() }, [])
+
   return (
     <main className=" bg-zinc-50 text-zinc-900 py-4 px-6 flex justify-center flex-col items-center">
       <div className="absolute top-4 left-4">
         <h1 className="font-bold text-4xl">Solrac News</h1>
       </div>
       <div className=" p-4 h-screen flex items-center flex-col space-y-4 max-w-[80%] my-20 w-max">
-        {news.map((content) => 
-        <div key={content.url} className="max-w-sm w-full h-64 lg:max-w-full lg:flex">
-          {/* <div
+        {news.map((content) =>
+          <div key={content.url} className="max-w-sm w-full h-64 lg:max-w-full lg:flex">
+            {/* <div
             className={`h-48 w-48 rounded-l-sm lg:h-auto lg:w-48 flex-none bg-cover bg-center text-center overflow-hidden bg-[url("${content.urlToImage}")]`}>
           </div> */}
-          <div className="border border-indigo-600 lg:border-indigo-600 bg-white w-full rounded-md  p-4 flex flex-col justify-between leading-normal">
-            <div className=" flex flex-col space-y-4">
-              <div>
-                <div className="text-gray-900 font-bold text-xl">{content.title}</div>
+            <div className="border border-indigo-600 lg:border-indigo-600 bg-white w-full rounded-md  p-4 flex flex-col justify-between leading-normal">
+              <div className=" flex flex-col space-y-4">
+                <div>
+                  <div className="text-gray-900 font-bold text-xl">{content.title}</div>
+                </div>
+                <p className="text-gray-700 line-clamp-3 text-justify text-base">{content.description}</p>
               </div>
-              <p className="text-gray-700 line-clamp-3 text-justify text-base">{content.description}</p>
-            </div>
-            <div className="flex justify-between w-full items-center">
-              <div className="text-md">
-                <p className="text-gray-900 leading-none">{content.author}</p>
-                <p className="text-gray-600">{format(content.publishedAt, "d' de 'MMMM", {locale : ptBR})}</p>
-              </div>
-              <div className="mx-5 relative self-end">
-                <a href={content.url}
-                  className=" shadow-shape font-semibold py-2 px-5 bg-indigo-600 rounded-md text-zinc-50 hover:bg-indigo-400 transition-all">Read</a>
+              <div className="flex justify-between w-full items-center">
+                <div className="text-md">
+                  <p className="text-gray-900 leading-none">{content.author}</p>
+                  <p className="text-gray-600">{format(content.publishedAt, "d' de 'MMMM", { locale: ptBR })}</p>
+                </div>
+                <div className="mx-5 relative self-end">
+                  <a href={content.url}
+                    className=" shadow-shape font-semibold py-2 px-5 bg-indigo-600 rounded-md text-zinc-50 hover:bg-indigo-400 transition-all">Read</a>
+                </div>
               </div>
             </div>
           </div>
-        </div>
         )}
       </div>
     </main>
